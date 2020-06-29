f6fa380381cdd7965b07c71f7b5a1fb625fd7b27
Abigail Grundy <abigial.grundy@bbc.co.uk>
2020-06-29 08:03:03 +0100

    refactor text file to push users into an array

--- index.js
+++ index.js
@@ -2,5 +2,19 @@ var fs = require("fs");
 
 fs.readFile("Transactions2014.csv", function (err, data) {
   let transactionsData = data.toString();
-  console.log(transactionsData);
+
+  //store all single lined transactions
+  let transactionsDataArray = transactionsData.split("\n");
+
+  let users = [];
+  //find the users in the transactionsDataArray
+  for (i = 0; i < transactionsDataArray.length; i++) {
+    let firstItem = transactionsDataArray[i].split(",");
+    console.log(firstItem);
+
+    let user1 = firstItem[1];
+    let user2 = firstItem[2];
+    users.push(user1, user2);
+  }
+  console.log(users);
 });
