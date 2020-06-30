f979123a70cc25459f6bf02b00d0cb31e20f5b63
Abigail Grundy <abigial.grundy@bbc.co.uk>
2020-06-30 10:34:54 +0100

    create a user object for each user

--- index.js
+++ index.js
@@ -5,16 +5,37 @@ fs.readFile("Transactions2014.csv", function (err, data) {
 
   //store all single lined transactions
   let transactionsDataArray = transactionsData.split("\n");
-
-  let users = [];
+  let existingArray = [];
+  let usersArray = [];
   //find the users in the transactionsDataArray
-  for (i = 0; i < transactionsDataArray.length; i++) {
-    let firstItem = transactionsDataArray[i].split(",");
-    console.log(firstItem);
+  for (i = 1; i < transactionsDataArray.length; i++) {
+    let item = transactionsDataArray[i].split(",");
+    //console.log(item);
 
-    let user1 = firstItem[1];
-    let user2 = firstItem[2];
-    users.push(user1, user2);
+    // take an proposed user already in the user array
+    let user1 = item[1];
+
+    let user2 = item[2];
+
+    // check too see if that user string, matches a string of the existing users
+    if (!usersArray.includes(user1)) {
+      // if it does not  match
+      usersArray.push(user1);
+    }
+
+    if (!usersArray.includes(user2)) {
+      // if it does not  match
+      usersArray.push(user2);
+    }
   }
+  // convert array of users into an array of accounts(object)
+  console.log(usersArray);
+
+  const users = usersArray.map(function (name) {
+    return {
+      name: name,
+      balance: 0,
+    };
+  });
   console.log(users);
 });
